a80548c21c8c8746143a88c99df8e85c3ad0b38f
Sergio Botero <sergio@ride.com>
2014-12-22 21:51:08 -0800

    Add dotfiles repository
    This is doing what most of the team does with their dotfiles.

--- manifests/dotfiles.pp
+++ manifests/dotfiles.pp
@@ -3,10 +3,27 @@
 class sergiobuj::dotfiles {
   notice('Setting up dotfiles for sergiobuj')
 
-  # This is just a test
-  #file { 'myvimrc':
-  #  ensure  => present,
-  #  path    => "/Users/${::boxen_user}/Desktop/myvimrc",
-  #}
+  $home     = "/Users/${::boxen_user}"
+  $src      = "${home}/src"
+  $dotfiles = "${src}/dotfiles"
+
+  file { $dotfiles:
+    ensure => directory
+  }
+
+  repository { $dotfiles:
+    source  => 'sergiobuj/dotfiles',
+    require => File[$dotfiles]
+  }
+
+  exec { 'installing dotfiles':
+    command => 'sh install.sh',
+    cwd     => $dotfiles
+  }
+
+  exec { 'pull dotfiles master':
+    command => 'git pull origin master',
+    cwd     => $dotfiles
+  }
 }
 
